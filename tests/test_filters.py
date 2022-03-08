84aa3e1939bf1599024084b97e0c50bf4fe4a3cf
Matt Hauff <quexington@gmail.com>
2022-03-08 12:38:24 -0700

    Set withdrawal timelock

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -66,6 +66,7 @@ async def setup_info():
         uint64(0),  # doesn't matter
         [ACS_PH],
         uint64(0),  # doesn't matter
+        uint64(0),  # doesn't matter
     )
 
     # Construct the two filters
