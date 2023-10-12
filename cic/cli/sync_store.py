cb8962449e9c282d6bced7b51ad805c77c5d6d6e
Chris Marslender <chrismarslender@gmail.com>
2023-10-12 13:02:18 -0500

    Fix the hex string in the sqlite query (needs to be x'<value>') (#64)

--- cic/cli/sync_store.py
+++ cic/cli/sync_store.py
@@ -286,9 +286,9 @@ class SyncStore:
         else:
             limit_str = ""
         async with self.db_wrapper.reader_no_transaction() as conn:
+            hex_amt = format(minimum_amount, "016x")
             cursor = await conn.execute(
-                f"SELECT * from p2_singletons WHERE amount>=? AND spent==0 ORDER BY amount DESC{limit_str}",
-                (format(minimum_amount, "016x"),),
+                f"SELECT * from p2_singletons WHERE amount>=x'{hex_amt}' AND spent==0 ORDER BY amount DESC{limit_str}"
             )
             coins = await cursor.fetchall()
             await cursor.close()
